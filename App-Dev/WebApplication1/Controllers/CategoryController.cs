c2dcdb88c5dea06e3426f13c26c164ee9b8a261d
NguyenVanThangzzzz <106645703+NguyenVanThangzzzz@users.noreply.github.com>
2023-11-21 17:13:16 +0700

    first commit

---
+++ App-Dev/WebApplication1/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext applicationDBContext)
+        {
+            _dbContext = applicationDBContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _dbContext.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _dbContext.Categories.Add(category);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ App-Dev/WebApplication1/Data/ApplicationDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+       public  DbSet<Category> Categories { get; set; }
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) //đưa tất cả options vào base
+        {
+
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Roman", Description = "ABCDEGH", DisplayOrder = 2 },
+                new Category { Id = 2, Name = "Action", Description = "GGGGG", DisplayOrder = 3 },
+                new Category { Id = 3, Name = "History", Description = "DDDDD", DisplayOrder = 4 }
+                );
+        }
+    }
+}
